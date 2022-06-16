3994383048416e59dc2cb8a03aa7f968c00a7133
Chris <christian.osterback@gmail.com>
2022-06-16 18:00:02 +0300

    yay

---
+++ index.js
@@ -0,0 +1,14 @@
+
+var video1 = document.getElementById("video1");
+
+// PLay Video2 on hover
+video2.addEventListener("mouseenter", function() {
+    video2.play();
+  });
+  
+  // Pause Video2 on hover
+  video2.addEventListener("mouseleave", function() {
+    video2.pause();
+  });
+
+
